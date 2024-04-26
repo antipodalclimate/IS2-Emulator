03d37b5ddb023140cecbd12edd42d7648731afd2
Christopher Horvat <christopher_horvat@brown.edu>
2024-04-25 22:41:20 -0400

    Orientation Angles Calculated using RGTs

--- Orbital-Incidence/get_orbit_kmls.m
+++ Orbital-Incidence/get_orbit_kmls.m
@@ -1,57 +1,60 @@
 % This code examines the angle of incidence of IS2 RGTs
 
-Data_folder = '../Data/Orbit-Data/'
-
-orbitlist = dir([Data_folder '*.kmz']);
-
-dummy_struct = kmz2struct([orbitlist(1).folder '/' orbitlist(1).name]);
+Data_folder = '../Data/Orbit-Data/';
 
 %% Load in all orbit RGTs
 
 earthellipsoid = referenceSphere('earth','km');
 
-for orbit_ind = 1:length(orbitlist)
+orbit_struct = kmz2struct([Data_folder 'Arctic_repeat1_GT7.kmz']);
 
-    fprintf('Orbit %d \n',orbit_ind); 
-    dummy_struct = kmz2struct([orbitlist(orbit_ind).folder '/' orbitlist(orbit_ind).name]);
+%%
+orientations = cell(1);
+latvals = cell(1);
 
+for i = 1:length(orbit_struct)
 
-    for i = 1:length(dummy_struct)
+    lats = orbit_struct(i).Lat;
+    lons = orbit_struct(i).Lon;
 
+    % Formula for the azimuth
+    % azimuth(lat1,lon1,lat2,lon2,ellipsoid)
+    % atan2(x,y)
+    % x = sin(dlon)cos(lat2)
+    % y = cos(lat1)sin(lat2) - sin(lat1)cos(lat2)cos(dlon)
 
-        lats = dummy_struct(i).Lat;
-        lons = dummy_struct(i).Lon;
-
-        % Formula for the azimuth
-        % azimuth(lat1,lon1,lat2,lon2,ellipsoid)
-        % atan2(x,y)
-        % x = sin(dlon)cos(lat2)
-        % y = cos(lat1)sin(lat2) - sin(lat1)cos(lat2)cos(dlon)
-
-        orientations{orbit_ind,i} = azimuth(lats(1:end-1),lons(1:end-1),lats(2:end),lons(2:end),earthellipsoid);
-        latvals{orbit_ind,i} = 0.5*(lats(1:end-1) + lats(2:end));
-
-    end
+    orientations{i} = azimuth(lats(1:end-1),lons(1:end-1),lats(2:end),lons(2:end),earthellipsoid);
+    latvals{i} = 0.5*(lats(1:end-1) + lats(2:end));
 
 end
-%% Form a PDF of incidence angle as a function of latitude
 
 orientations = cell2mat(orientations);
 
+% make orientations non-symmetric
+% Orientation 90 is direct W-E and same as 270
+% Orientation 180 is direct N-S and same as 360
+
+orientations(orientations > 180) = 360 - orientations(orientations > 180);
+
 latvals = cell2mat(latvals);
 
+%% Form a PDF of incidence angle as a function of latitude
+
+
 % Discretize to 0.1 degree
-latvals = floor(latvals*10)/10;
+latvals = floor(latvals*5)/5;
 
 [val,~,locs] = unique(latvals);
 
 neach = accumarray(locs,1);
 
+orients = 1:180; 
 
+for i = 1:length(val)
 
+    ohist(i,:) = histcounts(orientations(latvals == val(i)),orients,'Normalization','percentage');
 
-
-
+end
 
 %% Save this for use with the emulator
 
