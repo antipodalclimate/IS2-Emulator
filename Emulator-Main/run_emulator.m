86c72e2ebf21ba7380bb279e9c0246b219fa61f8
Christopher Horvat <christopher_horvat@brown.edu>
2024-04-30 19:03:03 -0400

    updated code as confirm 10x+ improved speed using parametric lines

--- Emulator-Main/run_emulator.m
+++ Emulator-Main/run_emulator.m
@@ -147,14 +147,16 @@ for i = 1:10
 
             xscale = nx/ny;
 
-            subplot('position',[.1 .4 xscale*.5 .5])
+            Ax_surf = subplot('position',[.1 .4 xscale*.5 .5]);
             imagesc(surface_class'); % show the image itself.
             set(gca,'YDir','normal')
             hold on
 
         end
 
-        parametric_lines = 1; 
+        parametric_lines = 0; 
+
+        measured_map = zeros(size(surface_class)); 
 
         for j = 1:n_crossings
 
@@ -184,11 +186,13 @@ for i = 1:10
             length_ice_measured(i,j) = sum(surface_class(coord_ind) == 1);
 
             if plotting == 1
+
             imagesc(measured_map')
             drawnow; 
             set(gca,'YDir','normal')
+
             end
-            
+
             else
 
                 % use only those points that are not nan for distance computation
@@ -201,17 +205,17 @@ for i = 1:10
                 % Length of ice measured is the close points that are ice
                 length_ice_measured(i,j) = sum(class_measurable(measured) == 1);
               
-                if plotting == 1
-                   
-                    scatter(sample_x(j),sample_y(j),150,'r','filled')
-                    plot([xinit(j) xend(j)],[yinit(j) yend(j)]);
-                    drawnow
 
-                end
             end
 
+            if plotting == 1
 
-          
+                scatter(Ax_surf,sample_x(j),sample_y(j),150,'r','filled')
+                plot(Ax_surf,[xinit(j) xend(j)],[yinit(j) yend(j)]);
+                drawnow
+
+            end
+
 
 
         end
@@ -232,8 +236,8 @@ for i = 1:10
             end
 
             plot(mean(est,2) - true_SIC(i),'k','linewidth',2); 
-            plot(std(est,[],2),'--','color',[.8 .8 .8]); 
-            plot(-std(est,[],2),'--','color',[.8 .8 .8]); 
+            plot(mean(est,2) - true_SIC(i) + std(est,[],2),'--','color',[.8 .8 .8]); 
+            plot(mean(est,2) - true_SIC(i) -std(est,[],2),'--','color',[.8 .8 .8]); 
             hold off
         end
 
